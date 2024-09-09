32bcc5fd3df47778892dd0d4fb96d9b36c369cb8
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-09-09 23:53:25 +0800

    feat : problem eror

--- backend/config/Databases.js
+++ backend/config/Databases.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("localhost", "root", "", {
+const db = new Sequelize("auth_db", "root", "", {
   host: "localhost",
   dialect: "mysql",
 });
--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,13 @@
 import express from "express";
+import db from "./config/Databases";
+
 const app = express();
+
+try {
+  await db.authenticate();
+  console.log("database connectd...");
+} catch (error) {
+  console.log(error);
+}
 
 app.listen(5000, () => console.log("server running in port 5000"));
