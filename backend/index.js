0f664d8e3de86efd92691939ef1135238ebe6a00
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-09-11 19:28:09 +0800

    feat : success send request

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import db from "./config/database.mjs";
+import router from "./routes/index.js";
 
 const app = express();
 
@@ -9,5 +10,8 @@ try {
 } catch (error) {
   console.log(error);
 }
+
+app.use(express.json());
+app.use(router);
 
 app.listen(5000, () => console.log("server running in port 5000"));
