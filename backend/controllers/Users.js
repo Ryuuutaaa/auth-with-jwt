00e79c06aad66a6cc8bf450af43e1fb073b3073e
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-09-14 11:11:26 +0800

    feat : add login

--- backend/controllers/Users.js
+++ backend/controllers/Users.js
@@ -1,5 +1,6 @@
 import Users from "../models/UserModel.mjs";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const getUsers = async (req, res) => {
   try {
@@ -28,5 +29,49 @@ export const Register = async (req, res) => {
     res.json({ msh: "Register berhasil" });
   } catch (error) {
     console.log(error);
+  }
+};
+
+export const Login = async (req, res) => {
+  try {
+    const user = await Users.findAll({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    const math = await bcrypt.compare(req.body.password, user[0].password);
+    if (!math) return res.status(400).json({ msg: "Wrong password" });
+
+    const userId = user[0].id;
+    const name = user[0].name;
+    const email = user[0].email;
+
+    const accessToken = jwt.sign(
+      { userId, name, email },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: "20s",
+      }
+    );
+
+    const refershToken = jwt.sign(
+      { userId, name, email },
+      process.env.REFRESH_TOKEN_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    await Users.update(
+      { refershToken: refershToken },
+      {
+        where: {
+          id: userId,
+        },
+      }
+    );
+  } catch (error) {
+    res.status(404).json({ msg: "Email tidak dapat ditemukan" });
   }
 };
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import db from "./config/database.mjs";
 import router from "./routes/index.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
 try {
