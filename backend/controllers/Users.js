500f737a05e0d53d090d070e51931f894cfb6ee2
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-09-14 10:43:07 +0800

    feat : add regiter and condition

--- backend/controllers/Users.js
+++ backend/controllers/Users.js
@@ -1,4 +1,5 @@
 import Users from "../models/UserModel.mjs";
+import bcrypt from "bcrypt";
 
 export const getUsers = async (req, res) => {
   try {
@@ -7,4 +8,12 @@ export const getUsers = async (req, res) => {
   } catch (error) {
     console.log(error);
   }
+};
+
+export const Register = async (req, res) => {
+  const { name, email, password, confPassword } = req.body;
+  if (password !== confPassword)
+    return res
+      .status(400)
+      .json({ msg: "Password dan Confirm Password tidak cocok" });
 };
